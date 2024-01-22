6f0d5b2f3609a522df4db94a52f2de8e9bd7aa01
jeffng5 <jeffreykng@hotmail.com>
2024-01-22 11:37:48 -0500

    updated latest changes

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
-import {v1 as uuid} from "uuid";
-import axios from "axios"
 import useAxios from "./hooks/useAxios"
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
-function CardTable() {
-  const [cards, setCards] = useState([]);
+const PlayingCardList = () => {
+  const [cards, setCards] = useState();
   
-    const getCard = useAxios(
-      "https://deckofcardsapi.com/api/deck/new/draw/",
-      
-      );
-      
+let url =  "https://deckofcardsapi.com/api/deck/new/draw/" 
+    const ans = useAxios(url);
     
-    setCards(cards => [...cards, { ...getCard.data, id: uuid() }]);
-    ;
+  console.log(ans)
+  console.log(ans)
+
+  setCards(ans['response']['data']['cards']['0']['image'])
   
   return (
+  
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={getCard}>Add a playing card!</button>
+        <button onClick={setCards}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
-        ))}
+        
+          <PlayingCard front={cards}/>  
+    
       </div>
     </div>
+  
   );
 }
 
-CardTable.defaultProps = {};
+// CardTable.defaultProps = {};
 
-export default CardTable;
+export default PlayingCardList;
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,20 +1,32 @@
 import { useState, useEffect } from "react";
 import axios from "axios"
 
-const useAxios = (url) => {
-    const [response, setResponse] = useState(null)
-    
-    
-   let getData =  async() => {
-        try{ 
-        const res = await axios.get(url);
-        setResponse(res)
-        }catch (error) {
-    
-        }
-        }
 
-    return {response}
+const useAxios = (url)  => {
+
+const [response, setResponse] = useState()
+const [error, setError] = useState()
+const [isLoading, setIsLoading] = useState(true)
+
+useEffect(() => {    
+const getData = () =>  {
+    try{    
+        let res = axios.get(url)
+        // const json = res.json()
+        console.log(res)
+        setResponse(res);
+        
+    } catch (error){
+        setError(error);
+    }
+    setIsLoading(false)
 }
+        getData();
+}, [url]);
+    console.log(response)
+
+    return {response, error, isLoading};
+};
+
 
 export default useAxios;
